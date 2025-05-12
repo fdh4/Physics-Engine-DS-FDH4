563d98c4e77064b2f6ad54afc3d6c7df2ffcafa4
Frank Howard <fdh4@aol.com>
2025-05-11 22:55:10 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,258 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const twoTT = 2*Math.PI; // note: I use the variable 'TT' to store 2*Pi
+const BALLZ = [];
+
+let LEFT, UP, RIGHT, DOWN;
+let friction = 0.1;
+
+class Vector{              // *** class Vector Begin ***
+  constructor(x, y){
+      this.x = x;
+      this.y = y; 
+  }
+
+  add(v){
+    return new Vector(this.x + v.x, this.y + v.y);
+  }
+
+  subtr(v){
+    return new Vector(this.x - v.x, this.y - v.y);
+  }
+
+  mag(){
+    return Math.sqrt(this.x**2 + this.y**2);
+  }
+
+  mult(n){
+    return new Vector(this.x*n, this.y*n);
+  }
+}                        //  *** class Vector End ***
+
+class Ball{              // *** class Ball Begin ***
+  constructor(x,y,r){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.vel = new Vector(0,0);
+    this.acc = new Vector(0,0);
+    this.acceleration = 0.1;
+    this.player = false;
+    BALLZ.push(this);
+  }
+
+  drawBall (){        // drawBall method of class Ball start
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, twoTT);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    ctx.fillStyle = "purple";
+    ctx.fill();
+  }                   // drawBall method of class Ball end
+
+  display(){
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x + this.acc_x*500, this.y + this.acc_y*500);
+    ctx.strokeStyle = "green";
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x + this.vel_x*50, this.y + this.vel_y*50);
+    ctx.strokeStyle = "blue";
+    ctx.stroke();
+
+  }
+}                        //  *** class Ball End ***
+
+function keyControl(b){         // *** function keyControl Begin ***
+
+  canvas.addEventListener('keydown', function(e){       //  *** keyDown listener Begin ***
+
+      let fdhIcode = e.key.charCodeAt();
+
+            // // console.log("fdhIcode =", fdhIcode);
+
+      if(fdhIcode == 115){LEFT = true;}
+
+      if(fdhIcode == 99){DOWN = true;}
+
+      if(fdhIcode == 101){UP = true;}
+        
+      if(fdhIcode == 102){RIGHT = true;}
+        
+    }
+  );                                                    // *** keyDown listener  End ***
+
+canvas.addEventListener('keyup', function(e){     // *** keyUp listener Begin ***
+   let fdhIcode = e.key.charCodeAt();
+
+     if (fdhIcode == 115){LEFT = false;}
+       
+     if (fdhIcode == 99){DOWN = false;}
+
+     if (fdhIcode == 101){UP = false;}
+
+     if (fdhIcode == 102){RIGHT = false;}
+    }
+);                                                // *** keyUp listener End ***
+
+      if (LEFT){
+        b.acc.x = -b.acceleration;
+      }
+
+      if (UP){
+        b.acc.y = -b.acceleration;
+      }
+
+      if (RIGHT){
+        b.acc.x = b.acceleration;
+      }
+
+      if (DOWN){
+        b.acc.y = b.acceleration;
+      }
+
+      if(!LEFT && !RIGHT){
+        b.acc.x = 0;
+      }
+  
+    if(!UP && !DOWN){
+      b.acc.y = 0;
+    }
+
+    b.vel = b.vel.add(b.acc);
+    b.vel = b.vel.mult(1-friction);
+    b.x += b.vel.x;
+    b.y += b.vel.y;
+
+}                               // *** function keyControl End ***
+    
+    function mainLoop() {       // *** function mainLoop Begin ***
+      ctx.clearRect(0,0,canvas.clientHeight, canvas.clientWidth);
+      BALLZ.forEach((b) => {
+        b.drawBall();
+        if (b.player){
+          keyControl(b);
+        }
+        b.display();
+      });
+      requestAnimationFrame(mainLoop);
+    }                           // *** function mainLoop End ***
+
+    let Ball1 = new Ball(canvas.width/2, canvas.height/2, 15);
+    // let Ball2 = new Ball(300, 400, 20);
+
+    Ball1.player = true;
+    // Ball2.player = false;
+
+    requestAnimationFrame(mainLoop);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+    // console.log("fdhIcode =", fdhIcode);
+    //  console.log(fdhIcode);
+
+      //
+      // Add a rectangle at (10, 10) with size 100x100 pixels
+      // ctx.fillRect(10, 10, 100, 100);
+      //
+
+      // console.log(text);
+      //  console.log();
+      // console.log("x = ",x);
+      // console.log("y = ", y);
+      // console.log();
+      // console.log();
+      // console.log("e.key.charCodeAt():");
+      // console.log(fdhIcode);
+      // console.log();   
+      //   console.log();
+      //   console.log("Key:  ");
+      // console.log(e.key);
+      //   console .log();
+      //   console.log("Code:  ");
+      // console.log(e.code);
+      //   console.log();
+      //   console.log("charCode:  ");
+      // console.log(e.key.charCodeAt()); 
+      //   console.log();
+      //   console.log();
+
+      //   let text = e.key;
+      //   let fdhIcode = e.key.charCodeAt();
+      // console.log(e.code.charCodeAt());
+      // console.log();
+      // console.log();
+  // if (fdhIcode == "100")
+  //   {
+  //      console.log("d => NONE");
+  //   }
+
+  //   if (fdhIcode == "101")
+  //   {
+  //        console.log("e => UP");
+  //       y--;
+  //   }
+
+  // if (fdhIcode == "99")
+  //   {
+  //      console.log("c => DOWN");
+  //     y++;
+  //   }
+
+  // if (fdhIcode == "115")
+  //   {
+  //     console.log("s => LEFT");
+  
+  //    x--;
+  //   }
+    
+  // if (fdhIcode == "102")
+  //   {
+  //    console.log("f => RIGHT");
+  //   x++;
+  //   }
+
+
+
